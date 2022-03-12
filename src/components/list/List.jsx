d89b6cd52a7ffdcd7431cc5b0a7aa4fb8c234fbf
Milton Rodrigues <rodriguesmilton21@gmail.com>
2022-03-12 21:46:30 +0530

    -

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -37,16 +37,16 @@ export default function List() {
           style={{ display: !isMoved && "none" }}
         ></ArrowBackIosOutlined>
         <div className="container" ref={listRef}>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
+          <ListItem index={0}></ListItem>
+          <ListItem index={1}></ListItem>
+          <ListItem index={2}></ListItem>
+          <ListItem index={3}></ListItem>
+          <ListItem index={4}></ListItem>
+          <ListItem index={5}></ListItem>
+          <ListItem index={6}></ListItem>
+          <ListItem index={7}></ListItem>
+          <ListItem index={8}></ListItem>
+          <ListItem index={9}></ListItem>
         </div>
         <ArrowForwardIosOutlined
           className="sliderArrow right"
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,12 +1,47 @@
 import "./listItem.scss";
+import {
+  PlayArrow,
+  Add,
+  ThumbDownOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
 
-export default function ListItem() {
+export default function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 }}
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
       <img
         src="https://m.media-amazon.com/images/M/MV5BOThjODMyM2QtNTNhYi00ZGM4LWIxZTAtMDAyYWNhYzYxMjJiXkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"
         alt=""
       ></img>
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow></PlayArrow>
+          <Add></Add>
+          <ThumbUpAltOutlined></ThumbUpAltOutlined>
+          <ThumbDownOutlined></ThumbDownOutlined>
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+        </div>
+        <div className="genre">Action</div>
+      </div>
     </div>
   );
 }
