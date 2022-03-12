badb43dfc281e46ba7df5a08fe6ba76f936a04d0
Milton Rodrigues <rodriguesmilton21@gmail.com>
2022-03-12 18:43:46 +0530

    -

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,12 @@
 import "./listItem.scss";
 
 export default function ListItem() {
-  return <div className="listItem">Item</div>;
+  return (
+    <div className="listItem">
+      <img
+        src="https://m.media-amazon.com/images/M/MV5BOThjODMyM2QtNTNhYi00ZGM4LWIxZTAtMDAyYWNhYzYxMjJiXkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"
+        alt=""
+      ></img>
+    </div>
+  );
 }
